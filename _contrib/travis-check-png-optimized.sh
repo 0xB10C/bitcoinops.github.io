b5770c10bc35fc53f06ab842e9123504604afaf5
0xb10c <0xb10c@gmail.com>
2019-08-14 13:09:43 +0200

    Add files for a travis test

---
+++ _contrib/travis-check-png-optimized.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+## Finds newly added or modifyed PNG files in the repository and checks if each file is optimized.
+## If not optimized it fails with a error code 1
+
+CHANGEDFILES=$(git diff --name-only --diff-filter=AM HEAD...$TRAVIS_BRANCH)
+
+for FILE in $CHANGEDFILES; do
+	if echo $FILE | grep -q ".png$"; then 
+		echo checking $FILE
+		OPTIPNGOUTPUT=$(optipng -simulate "$FILE" 2>&1)
+    	if ! echo $OPTIPNGOUTPUT | grep -q "is already optimized"; then
+        	echo "The file $FILE is not optimized. To optimize use 'optipng -o7 $FILE'."
+        	exit 1
+    	fi
+	fi
+done
